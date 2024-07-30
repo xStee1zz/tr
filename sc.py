ad612e1976e7caa5ca14dbd74776577800374cc4
xStee1zz <164562517+xStee1zz@users.noreply.github.com>
2024-07-30 22:08:29 +0300

    Update sc.py

--- sc.py
+++ sc.py
@@ -20,7 +20,7 @@ def like():
         }
 
         data = {
-            'friendId': 3269308288,
+            'friendId': 65980143,
             'msg': ''
         }
 
@@ -29,15 +29,15 @@ def like():
         url = "http://modsgs.sandboxol.com/friend/api/v1/friends/requests/approve-all"
 
         headers = {
-            'userId': '3269308288',
-            'Access-Token': 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzMjY5MzA4Mjg4IiwiaWF0IjoxNzIyMzYzNjE4LCJzdWIiOiIyMDI0MDczMCAxODIwMTg3MzMiLCJpc3MiOiJTYW5kYm94LVNlY3VyaXR5LUJhc2ljIiwiZXhwIjoxNzIyOTY4NDE4fQ.UwxDUuZ6JehFlpajI7NoVM78O9MxVP-ec5-6ikiM3SM',
+            'userId': '65980143',
+            'Access-Token': 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI2NTk4MDE0MyIsImlhdCI6MTcyMjM2MjY2NSwic3ViIjoiMjAyNDA3MzAgMTgwNDI1ODI4IiwiaXNzIjoiU2FuZGJveC1TZWN1cml0eS1CYXNpYyIsImV4cCI6MTcyMjk2NzQ2NX0.8f4OFu535pHBtW_nYH1wXCA6GAct_554FwX8Ub3pxxQ',
             'packageName': 'amosCraft',
             'User-Agent': 'okhttp/3.12.1'
         }
 
         response = requests.post(url, headers=headers)
 
-        url = "http://modsgs.sandboxol.com/friend/api/v1/popularity?friendId=3269308288"
+        url = "http://modsgs.sandboxol.com/friend/api/v1/popularity?friendId=65980143"
 
         headers = {
             'userId': bot_id,
@@ -54,7 +54,7 @@ def like():
         else:
             print(f"\033[1m\033[31m{message}\033[0m")
 
-        url = "https://gw.sandboxol.com/friend/api/v1/friends?friendId=3269308288"
+        url = "https://gw.sandboxol.com/friend/api/v1/friends?friendId=65980143"
 
         headers = {
             'userId': bot_id,
